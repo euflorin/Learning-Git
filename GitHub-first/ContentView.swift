8a333189d394b73205b1909625f72be52e1600a9
euflorin <44313587+euflorin@users.noreply.github.com>
2021-04-28 13:18:39 +0200

    Initial Commit

---
+++ GitHub-first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub-first
+//
+//  Created by Florin Nitu on 28.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub-first/GitHub_firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_firstApp.swift
+//  GitHub-first
+//
+//  Created by Florin Nitu on 28.04.21.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
